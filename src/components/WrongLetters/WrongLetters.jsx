caa2bf770e5807bf14a2413de4366e2fdfae57dc
Maria Valero <mvalerogonzalez@gmail.com>
2021-07-14 15:05:11 +0100

    coding for Wrong Letters

---
+++ src/components/WrongLetters/WrongLetters.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const WrongLetters = ({wrongLetters}) => {
+  
+  export default function WrongLetters({wrongLetters}) {
+    return (
+      <div classname ="wrong-letters-container">
+        <div>
+          {wrongLetters.length > 0 && 
+            <p>Wrong</p>
+          }
+          {wrongLetters
+            .map((letter,i) => <span key={i}>{letter}</span>)
+            .reduce((prev,curr) => prev === null ? [curr] : [prev,', ', curr], null)}
+        </div>
+      </div>
+    )
+  }  
+}
+
