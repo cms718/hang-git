64e5a77dee691b822426182499ffac4cee96597c
Charlie Slater <charliemslater1701@gmail.com>
2021-07-14 12:59:52 +0100

    Added passing tests

---
+++ src/components/__tests__/HiddenWord.test.js
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react";
+import HiddenWord from "../HiddenWord";
+
+test("renders all underscores if no guessed letter", () => {
+  const word = "init";
+  const guessedLetters = [];
+  render(<HiddenWord word={word} guessedLetters={guessedLetters} />);
+  const letters = screen.getAllByText("_");
+  expect(letters.length).toBe(4);
+});
+
+test("renders an _ for each unknown letter", () => {
+  const word = "init";
+  const guessedLetters = ["i"];
+  render(<HiddenWord word={word} guessedLetters={guessedLetters} />);
+  const letters = screen.getAllByText("_");
+  const letters2 = screen.getAllByText("i");
+  expect(letters.length).toBe(2);
+  expect(letters2.length).toBe(2);
+});
