7ac8a1bbc624e513fb4458b5bc02194921aef00e
“ells101” <ells101@hotmail.co.uk>
2021-07-15 17:12:44 +0100

    second test to account for repeat incorrect word

--- src/components/__tests__/Notification.test.js
+++ src/components/__tests__/Notification.test.js
@@ -18,27 +18,16 @@ test("checks if letter has been repeated on a correct letter", () => {
   fireEvent.keyDown(document, { key: "i" });
   const myNotification = screen.getByText(/You have already entered this letter/i);
   expect(myNotification).toBeInTheDocument();
-
-  // expect(lives).toBeInTheDocument("5");
-
 });
 
-// test("checks if letter has been repeated on an in-correct letter", () => {
-//   render(<App />);
+test("checks if letter has been repeated on an incorrect letter", () => {
+  render(<App />);
 
-//   const lives = screen.getByText("6");
-//   expect(lives).toBeInTheDocument();
-//   fireEvent.keyDown(document, { key: "i" });
-//   const letters = screen.getAllByText("_");
-//   const correctLetters = screen.getAllByText("i");
-//   // expect(letters.length).toBe(2);
-//   // expect(correctLetters.length).toBe(2);
-
-
-//   fireEvent.keyDown(document, { key: "i" });
-//   const myNotification = screen.getByText(/You have already entered this letter/i);
-//   expect(myNotification).toBeInTheDocument();
-
-//   // expect(lives).toBeInTheDocument("5");
-
-// });
+ const lives = screen.getByText("6");
+  expect(lives).toBeInTheDocument();
+  fireEvent.keyDown(document, { key: "a" });
+  expect(lives).toBeInTheDocument("5");
+  fireEvent.keyDown(document, { key: "a" });
+  const myNotification = screen.getByText(/You have already entered this letter/i);
+  expect(myNotification).toBeInTheDocument();
+});
