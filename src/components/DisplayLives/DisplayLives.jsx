d423d60f3d2a21ffe9b996a0ad17dec3f7449514
Russell Horth <russellhorth@googlemail.com>
2021-07-16 15:18:59 +0100

    testing stickman passes

--- src/components/DisplayLives/DisplayLives.jsx
+++ src/components/DisplayLives/DisplayLives.jsx
@@ -22,15 +22,15 @@ export default function DisplayLives({word, guessedLetters, lives, updateLives})
         <line x1="20" y1="230" x2="100" y2="230" />
 
         {/* <!-- Head --> */}
-        {lives <= 5  && <circle cx="140" cy="70" r="20"/>}
+        {lives <= 5  && <circle cx="140" cy="70" r="20" data-testid="head"/>}
         {/* <!-- Body --> */}
-        {lives <= 4 && <line x1="140" y1="90" x2="140" y2="150"/>}
+        {lives <= 4 && <line x1="140" y1="90" x2="140" y2="150" data-testid="body"/> }
         {/* <!-- Arms --> */}
-        {lives <= 3 && <line x1="140" y1="120" x2="120" y2="100"/>}
-        {lives <= 2 && <line x1="140" y1="120" x2="160" y2="100"/>}
+        {lives <= 3 && <line x1="140" y1="120" x2="120" y2="100" data-testid="arm1"/>}
+        {lives <= 2 && <line x1="140" y1="120" x2="160" y2="100" data-testid="arm2"/>}
         {/* <!-- Legs --> */}
-        {lives <= 1 && <line x1="140" y1="150" x2="120" y2="180"/>}
-        {lives <= 0 && <line x1="140" y1="150" x2="160" y2="180"/>}
+        {lives <= 1 && <line x1="140" y1="150" x2="120" y2="180" data-testid="leg1"/>}
+        {lives <= 0 && <line x1="140" y1="150" x2="160" y2="180" data-testid="leg2"/>}
   </svg>
       <br></br>
       <span>Lives remaining:</span> <span>{lives}</span>
