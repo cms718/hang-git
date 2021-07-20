9aa83c7a444533ccec6608d82ad2333a5d09af7d
Charlie Slater <charliemslater1701@gmail.com>
2021-07-20 15:19:11 +0100

    Correct functionality for words with spaces

--- src/components/HiddenWord/HiddenWord.jsx
+++ src/components/HiddenWord/HiddenWord.jsx
@@ -5,6 +5,9 @@ export default function HiddenWord({word, guessedLetters, inProgress}) {
   return (
     <div className="hidden-word-container">
       {word.split("").map((letter, index) => {
+        if(letter === " ") {
+          return <p key={`id-${index}`} className="space"></p> 
+        }
         if(guessedLetters.includes(letter) || !inProgress) {
           return <p key={`id-${index}`} className="letter">{letter}</p> 
         } else {
