7372f725f91702bcff88edcc20be349302ed7d8c
Charlie Slater <charliemslater1701@gmail.com>
2021-07-21 17:47:02 +0100

    Fetches players from backend

--- src/components/ScoreBoard/ScoreBoard.jsx
+++ src/components/ScoreBoard/ScoreBoard.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
-//userScores currently hardcoded, needs database
+import React, { useState, useEffect } from 'react'
+import { userScores } from '../../UserScores';
 
 export default function ScoreBoard({navigateBack, userScores}) {
+  const [players, setPlayers] = useState(userScores)
+
+  const URL = process.env.REACT_APP_URL || "localhost:5000/players"
+
+  useEffect(() => {
+    const getPlayers = async () => {
+      const res = await fetch(URL)
+      const data = await res.json()
+      setPlayers(data)
+    }
+    getPlayers()
+  }, [URL])
+
+  if (!players) return "Error Loading Players"
   return (
     <div>
       <button onClick={navigateBack}>Back</button>
-      <h1>High Scores</h1>
+      <h1>High Scores: </h1>
       <ol>
-      {userScores.map((player) => {
+      {players.map((player) => {
         return <li key={player.username}>{player.username.toUpperCase()} : {player.score}</li>
       })}
       </ol>
