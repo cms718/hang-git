b2b77c23e5cad39f5864dd2be72723cbecd167df
BecaLParker <becaparker@gmail.com>
2021-07-21 12:15:57 +0100

    A5 each of easy, medium and hard

--- src/QuestionData.js
+++ src/QuestionData.js
@@ -1,32 +1,83 @@
 export const questionData = [
+  // EASY
   { 
-  word: "init", 
-  level: "easy", 
-  hint: "initialise git" 
+    word: "init", 
+    level: "easy", 
+    hint: "initialise git" 
   },
   {
     word: "status",
     level: "easy",
-    hint: "see which files have changed on your local version since the last commit",
+    hint: "show which files have changed on your local version since the last commit",
   },
   {
     word: "pull",
     level: "easy",
-    hint: "update what’s on your local version to match what’s on the Github version",
+    hint: "update what’s on your local version to match what’s on the GitHub version",
   },
   {
     word: "diff",
     level: "easy",
-    hint: "see what has changed within files",
+    hint: "show what has changed within files",
   },
+  {
+    word: "push",
+    level: "easy",
+    hint: "send any committed changes to the remote version of your branch",
+  },
+
+  // MEDIUM
+  { 
+    word: "clone <url>", 
+    level: "medium", 
+    hint: "make a local copy of a repo that’s on GitHub" 
+  },
+  { 
+    word: "log", 
+    level: "medium", 
+    hint: "display the entire commit history" 
+  },
+  { 
+    word: "add .", 
+    level: "medium", 
+    hint: "stage all changes in the current directory for the next commit" 
+  },
+  { 
+    word: "commit -m “<message>”", 
+    level: "medium", 
+    hint: "commit the staged snapshot" 
+  },
+  { 
+    word: "commit -am “<message>”", 
+    level: "medium", 
+    hint: "stage and commit changes in one step" 
+  },
+
+
+  // HARD
   {
     word: "branch",
     level: "medium",
     hint: "list all the branches in your repo",
   },
   {
+    word: "branch z",
+    level: "hard",
+    hint: "create a new branch named 'z', but don’t move into it yet",
+  },
+  {
+    word: "switch z",
+    level: "hard",
+    hint: "move to an existing branch named 'z'",
+  },
+  {
     word: "checkout -b z",
     level: "hard",
-    hint: "create and begin working on a new branch 'z'",
+    hint: "create and begin working on a new branch 'z' in one step",
+  },
+  {
+    word: "branch -d z",
+    level: "hard",
+    hint: "delete a branch named 'z' from your local version of the repo",
   },
 ];
