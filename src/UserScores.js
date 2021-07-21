669b7ee10a79e5135b422b362b2412ccf97f92bf
BecaLParker <becaparker@gmail.com>
2021-07-21 15:16:02 +0100

    Initial hookupusing array format

---
+++ src/UserScores.js
@@ -0,0 +1,11 @@
+// hardcoded for now but will be an API call to our database, once built
+export const userScores = [
+  [ 
+    "octokitty", 
+    100
+  ],
+  [
+    "buck-git list", 
+    50 
+  ]
+];
--- src/components/ScoreBoard/ScoreBoard.jsx
+++ src/components/ScoreBoard/ScoreBoard.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import {userScores} from '../../UserScores'
 
 export default function ScoreBoard({navigateBack}) {
   return (
     <div>
       <button onClick={navigateBack}>Back</button>
       <h1>High Scores: </h1>
+      
+      {userScores}
+      
+      {/* {/ Map over and return list of <Player /> components /}
+      {userScores.map((player) => {
+      return <User />
+      })} */}
     </div>
   )
 }
