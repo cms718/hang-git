6e36afda4d202cb3e7dbce83100de9927ceb7a7c
BecaLParker <becaparker@gmail.com>
2021-07-21 11:13:04 +0100

    extracts exisiting hardcoded data

--- src/App.js
+++ src/App.js
@@ -3,37 +3,9 @@ import "./App.css";
 import Game from "./components/Game/Game";
 import HomeScreen from "./components/HomeScreen/HomeScreen";
 import ScoreBoard from "./components/ScoreBoard/ScoreBoard";
+import {fakeData} from "./QuestionData.js"
 
 function App() {
-  // Replace fakaData with a fetch to API in a useEffect with async function
-  const fakeData = [
-    { word: "init", level: "easy", hint: "initialise git" },
-    {
-      word: "status",
-      level: "easy",
-      hint: "see which files have changed on your local version since the last commit",
-    },
-    {
-      word: "pull",
-      level: "easy",
-      hint: "update what’s on your local version to match what’s on the Github version",
-    },
-    {
-      word: "diff",
-      level: "easy",
-      hint: "see what has changed within files",
-    },
-    {
-      word: "branch",
-      level: "medium",
-      hint: "list all the branches in your repo",
-    },
-    {
-      word: "checkout -b z",
-      level: "hard",
-      hint: "create and begin working on a new branch 'z'",
-    },
-  ];
 
   const [gameStarted, setGameStarted] = useState(false);
   const [user, setUser] = useState({ name: "Anon", score: 0 });
---
+++ src/QuestionData.js
@@ -0,0 +1,28 @@
+export const fakeData = [
+  { word: "init", level: "easy", hint: "initialise git" },
+  {
+    word: "status",
+    level: "easy",
+    hint: "see which files have changed on your local version since the last commit",
+  },
+  {
+    word: "pull",
+    level: "easy",
+    hint: "update what’s on your local version to match what’s on the Github version",
+  },
+  {
+    word: "diff",
+    level: "easy",
+    hint: "see what has changed within files",
+  },
+  {
+    word: "branch",
+    level: "medium",
+    hint: "list all the branches in your repo",
+  },
+  {
+    word: "checkout -b z",
+    level: "hard",
+    hint: "create and begin working on a new branch 'z'",
+  },
+];
