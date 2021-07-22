a6eb9e9971ffc93fa8146a4ab15570b2112a11ff
BecaLParker <becaparker@gmail.com>
2021-07-22 14:04:19 +0100

    checks body parts persist once present

--- src/components/__tests__/DisplayLives.test.js
+++ src/components/__tests__/DisplayLives.test.js
@@ -27,23 +27,35 @@ test("lives deplete by 1 with each incorrect guess", () => {
 
 test("Head is displayed", () => {
   fireEvent.keyDown(document, { key: "Q" });
-  const head = screen.getByTestId("head");
+  const head = screen.queryByTestId("head");
+  const body = screen.queryByTestId("body");
   expect(head).toBeInTheDocument();
+  expect(body).not.toBeInTheDocument();
 });
 
 test("Body is displayed", () => {
   fireEvent.keyDown(document, { key: "W" });
   fireEvent.keyDown(document, { key: "Q" });
-  const body = screen.getByTestId("body");
+  const head = screen.queryByTestId("head");
+  const body = screen.queryByTestId("body");
+  const arm1 = screen.queryByTestId("arm1");
+  expect(head).toBeInTheDocument();
   expect(body).toBeInTheDocument();
+  expect(arm1).not.toBeInTheDocument();
 });
 
 test("Arm1 is displayed", () => {
   fireEvent.keyDown(document, { key: "W" });
   fireEvent.keyDown(document, { key: "Q" });
   fireEvent.keyDown(document, { key: "R" });
-  const arm1 = screen.getByTestId("arm1");
+  const head = screen.queryByTestId("head");
+  const body = screen.queryByTestId("body");
+  const arm1 = screen.queryByTestId("arm1");
+  const arm2 = screen.queryByTestId("arm2");
+  expect(head).toBeInTheDocument();
+  expect(body).toBeInTheDocument();
   expect(arm1).toBeInTheDocument();
+  expect(arm2).not.toBeInTheDocument();
 });
 
 test("Arm2 is displayed", () => {
@@ -51,8 +63,16 @@ test("Arm2 is displayed", () => {
   fireEvent.keyDown(document, { key: "Q" });
   fireEvent.keyDown(document, { key: "R" });
   fireEvent.keyDown(document, { key: "Y" });
-  const arm2 = screen.getByTestId("arm2");
+  const head = screen.queryByTestId("head");
+  const body = screen.queryByTestId("body");
+  const arm1 = screen.queryByTestId("arm1");
+  const arm2 = screen.queryByTestId("arm2");
+  const leg1 = screen.queryByTestId("leg1");
+  expect(head).toBeInTheDocument();
+  expect(body).toBeInTheDocument();
+  expect(arm1).toBeInTheDocument();
   expect(arm2).toBeInTheDocument();
+  expect(leg1).not.toBeInTheDocument();
 });
 
 test("Leg1 is displayed", () => {
@@ -61,8 +81,19 @@ test("Leg1 is displayed", () => {
   fireEvent.keyDown(document, { key: "R" });
   fireEvent.keyDown(document, { key: "Y" });
   fireEvent.keyDown(document, { key: "U" });
-  const leg1 = screen.getByTestId("leg1");
+  const head = screen.queryByTestId("head");
+  const body = screen.queryByTestId("body");
+  const arm1 = screen.queryByTestId("arm1");
+  const arm2 = screen.queryByTestId("arm2");
+  const leg1 = screen.queryByTestId("leg1");
+  const leg2 = screen.queryByTestId("leg2");
+  expect(head).toBeInTheDocument();
+  expect(body).toBeInTheDocument();
+  expect(arm1).toBeInTheDocument();
+  expect(arm2).toBeInTheDocument();
   expect(leg1).toBeInTheDocument();
+  expect(leg2).not.toBeInTheDocument();
+
 });
 
 test("Leg2 is displayed", () => {
@@ -72,6 +103,16 @@ test("Leg2 is displayed", () => {
   fireEvent.keyDown(document, { key: "Y" });
   fireEvent.keyDown(document, { key: "U" });
   fireEvent.keyDown(document, { key: "O" });
-  const leg2 = screen.getByTestId("leg2");
+  const head = screen.queryByTestId("head");
+  const body = screen.queryByTestId("body");
+  const arm1 = screen.queryByTestId("arm1");
+  const arm2 = screen.queryByTestId("arm2");
+  const leg1 = screen.queryByTestId("leg1");
+  const leg2 = screen.queryByTestId("leg2");
+  expect(head).toBeInTheDocument();
+  expect(body).toBeInTheDocument();
+  expect(arm1).toBeInTheDocument();
+  expect(arm2).toBeInTheDocument();
+  expect(leg1).toBeInTheDocument();
   expect(leg2).toBeInTheDocument();
 });
